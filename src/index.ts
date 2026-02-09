33bd7fd39323b9adf40f261394e95d6ba4087f93
Lars van Vianen <lars@vianen.com>
2026-02-09 01:36:32 +0100

    feat: add registerActions for plugin system v1.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,30 @@
-import { JestAction } from "./actions/JestAction/index.js";
+// ============================================================================
+// Export
+// ============================================================================
 
 export { JestAction } from "./actions/JestAction/index.js";
 export type { JestActionOptions } from "./actions/JestAction/index.js";
-export { Action } from "./types/Action.js";
+export { Action, ActionPlugin } from "./types/Action.js";
+export type { ActionOptionsType } from "./types/Action.js";
 
-/**
- * Plugin definition for kist
- */
-export default {
-    name: "@getkist/action-jest",
+// ============================================================================
+// Plugin Definition
+// ============================================================================
+
+import { ActionPlugin } from "./types/Action.js";
+import { JestAction } from "./actions/JestAction/index.js";
+
+const plugin: ActionPlugin = {
     version: "1.0.0",
-    actions: {
-        JestAction: new JestAction(),
+    description: "Jest test runner integration for kist",
+    author: "kist",
+    repository: "https://github.com/getkist/kist-action-jest",
+    keywords: ["kist", "kist-action", "jest", "test"],
+    registerActions() {
+        return {
+            JestAction,
+        };
     },
 };
+
+export default plugin;
